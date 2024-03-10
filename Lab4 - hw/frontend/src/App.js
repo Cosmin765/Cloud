349f243586e6998a1f1e2013e7eb55b1ac71fd7c
Cosmin Turtureanu <cturtureanu@bitdefender.com>
2024-03-10 15:50:49 +0200

    add more info slide

--- Lab4 - hw/frontend/src/App.js
+++ Lab4 - hw/frontend/src/App.js
@@ -5,34 +5,54 @@ import PilotCard from './components/PilotCard';
 
 export default function App() {
   const pilots = [
-    {
-        "_id": "65ea2e2849ef9a00e0dcd558",
-        "first_name": "Max",
-        "last_name": "Verstappen",
-        "score": 51,
-        "image_url": "https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/M/MAXVER01_Max_Verstappen/maxver01.png.transform/2col/image.png"
-    },
-    {
-        "_id": "65ea2e2849ef9a00e0dcd559",
-        "first_name": "Sergio",
-        "last_name": "Perez",
-        "score": 36,
-        "image_url": "https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/S/SERPER01_Sergio_Perez/serper01.png.transform/2col/image.png"
-    },
-    {
-        "_id": "65ea2e2849ef9a00e0dcd560",
-        "first_name": "Charles",
-        "last_name": "Leclerc",
-        "score": 28,
-        "image_url": "https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/C/CHALEC01_Charles_Leclerc/chalec01.png.transform/2col/image.png"
-    },
-    {
-        "_id": "65ea2e2849ef9a00e0dcd561",
-        "first_name": "Alexander",
-        "last_name": "Albon",
-        "score": 0,
-        "image_url": "https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/A/ALEALB01_Alexander_Albon/alealb01.png.transform/2col/image.png"
-    },
+        {
+            "_id": "65ea2e2849ef9a00e0dcd558",
+            "first_name": "Max",
+            "last_name": "Verstappen",
+            "score": 51,
+            "image_url": "https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/M/MAXVER01_Max_Verstappen/maxver01.png.transform/2col/image.png",
+            "extra_info": {
+                "team": "Red Bull Racing",
+                "country": "Netherlands",
+                "podiums": 100
+            }
+        },
+        {
+            "_id": "65ea2e2849ef9a00e0dcd559",
+            "first_name": "Sergio",
+            "last_name": "Perez",
+            "score": 36,
+            "image_url": "https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/S/SERPER01_Sergio_Perez/serper01.png.transform/2col/image.png",
+            "extra_info": {
+                "team": "Red Bull Racing",
+                "country": "Mexico",
+                "podiums": 260
+            }
+        },
+        {
+            "_id": "65ea2e2849ef9a00e0dcd560",
+            "first_name": "Charles",
+            "last_name": "Leclerc",
+            "score": 28,
+            "image_url": "https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/C/CHALEC01_Charles_Leclerc/chalec01.png.transform/2col/image.png",
+            "extra_info": {
+                "team": "Ferrari",
+                "country": "Monaco",
+                "podiums": 31
+            }
+        },
+        {
+            "_id": "65ea2e2849ef9a00e0dcd561",
+            "first_name": "Alexander",
+            "last_name": "Albon",
+            "score": 0,
+            "image_url": "https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/A/ALEALB01_Alexander_Albon/alealb01.png.transform/2col/image.png",
+            "extra_info": {
+                "team": "Williams",
+                "country": "Thailand",
+                "podiums": 2
+            }
+        },
   ];
 
   return (
--- Lab4 - hw/frontend/src/components/PilotCard.js
+++ Lab4 - hw/frontend/src/components/PilotCard.js
@@ -5,27 +5,38 @@ import { useEffect, useState } from 'react';
 
 export default function PilotCard({pilot}) {
     const [backgroundColor, setBackgroundColor] = useState('white');
-    const [textColor, setTextColor] = useState('black');
+    const [dark, setDark] = useState(true);
 
     useEffect(() => {
         new FastAverageColor().getColorAsync(pilot.image_url)
         .then(color => {
             const v = color.value.map(v => 255 - v);
             setBackgroundColor(`rgb(${v[0]}, ${v[1]}, ${v[2]})`);
-            setTextColor(color.isDark ? 'black' : 'white');
+            setDark(color.isDark);
         })
         .catch(e => console.log(e));
     }, []);
 
     return (
-        <div className='pilotCard' style={{backgroundColor, color: textColor}}>
-            <img className='profileImg' src={pilot.image_url} />
-            <div style={{display: 'flex', justifyContent: 'space-between'}}>
-                <div className='nameGroup'>
-                    <p className='firstName'>{pilot.first_name}</p>
-                    <p className='lastName'>{pilot.last_name}</p>
+        <div className='pilotCard' style={{backgroundColor, color: dark ? 'black' : 'white'}}>
+            <div className='content'>
+                <img style={{width: '100%'}} src={pilot.image_url} />
+                <div style={{display: 'flex', justifyContent: 'space-between'}}>
+                    <div className='nameGroup'>
+                        <p className='firstName'>{pilot.first_name}</p>
+                        <p className='lastName'>{pilot.last_name}</p>
+                    </div>
+                    <p style={{fontWeight: 'bold'}}>Score: {pilot.score}</p>
                 </div>
-                <p className='score'>Score: {pilot.score}</p>
+            </div>
+
+            <div className='moreInfo' style={{backgroundColor}}>
+                {Object.entries(pilot.extra_info).map(([key, value]) => {
+                    return <div style={{display: 'flex', justifyContent: 'space-between'}}>
+                        <p>{key}</p>
+                        <p>{value}</p>
+                    </div>;
+                })}
             </div>
         </div>
     );
